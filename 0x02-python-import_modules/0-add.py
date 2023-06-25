ad8fb57bc752ffb81cad6dd244c8ff5716f790ee
lafertd <achrafprofessionalinfo@gmail.com>
2023-06-25 09:55:23 -0700

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+    if __name__ == "__main__":
 
     from add_0 import add
 
@@ -6,4 +7,3 @@
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
-    if __name__ == "__main__":
