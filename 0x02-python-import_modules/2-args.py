2c274bd1362a649f38c6cd50da1ef65ce268e345
Lafertd <43250553+Lafertd@users.noreply.github.com>
2023-06-27 03:38:14 +0100

    up

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     from sys import argv
-
-    if len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
-
-    elif len(argv) == 2:
-        print("{} argument:".format(len(argv) - 1))
-        print("1: {} ".format(argv[1]))
-
+    n = len(argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
     else:
-        print("{} arguments:".format(len(argv) - 1))
-        for index, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(index, arg))
+        print("{} arguments:".format(n))
+    for i in range(n):
+        print("{}: {}".format(i + 1, argv[i + 1]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    if len(argv) == 1:
-        print("0")
-    else:
-        print("{}".format(int(argv)))
+    count = len(argv) - 1
+    n = 0
+    for i in range(count):
+        n += int(argv[i + 1])
+    print(n)
