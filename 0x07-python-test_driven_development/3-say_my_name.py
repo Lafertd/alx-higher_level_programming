ddad8ca97972d8cb11d3fb9ba0bd58b262196f3f
lafertd <achrafprofessionalinfo@gmail.com>
2024-04-27 15:28:28 -0700

    updated

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
 """
-This module returs your full name
+This module returns your full name.
 """
+
 def say_my_name(first_name, last_name=""):
     """
-    this function takes 2 strings and returns the full name
+    This function takes two strings and returns the full name.
 
-    args:
-    first_name: string
-    last_name: string
+    Args:
+        first_name (str): The first name.
+        last_name (str): The last name. Defaults to an empty string.
 
-    raises: 
-    raise a TypeError if the input is not string
+    Raises:
+        TypeError: If the input is not a string.
 
-    return:
-    retuns the full name of the person
-
+    Returns:
+        str: The full name of the person.
     """
-    if first_name is not type(str):
+    if not isinstance(first_name, str):
         raise TypeError("TypeError: the first_name must be a string")
-    elif last_name is not type(str):
+    elif not isinstance(last_name, str):
         raise TypeError("TypeError: the last_name must be a string")
-    else
-    print("My name is", <first name> <last name>)
-    return first_name + last_name
+    else:
+        print("My name is", first_name + " " + last_name)
+        return first_name + " " + last_name
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/3-say_my_name.txt")
+
