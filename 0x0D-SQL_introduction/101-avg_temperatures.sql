d140d68e2ebc9d883303980aa6121c453f0c4bd6
lafertd <achrafprofessionalinfo@gmail.com>
2024-12-02 03:27:56 +0100

    updated

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,2 +1,2 @@
 -- Avg temperature in f
-SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
