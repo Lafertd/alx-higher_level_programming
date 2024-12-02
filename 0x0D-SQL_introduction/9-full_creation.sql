f597dcc7de7c8410ad553d088b8c103003450afd
lafertd <achrafprofessionalinfo@gmail.com>
2024-12-02 01:43:19 +0100

    updated

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- Create a Table with multiple rows
 CREATE TABLE IF NOT EXISTS second_table (
-	id INT
+	id INT,
 	name VARCHAR(256),
 	score INT
-	)
-INSERT INTO second_table id, name, score VALUES (1, 'John', 10) (2, 'Alex', 3) (3, 'Bob', 14) (4, 'George', 8);
+);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
