5f21e07c710ed4512de71d16e30514fa1dc3b0af
lafertd <achrafprofessionalinfo@gmail.com>
2024-12-02 02:47:48 +0100

    updated

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- List the number of records with the same score
 SELECT `score`, COUNT(`score`) as `number` 
-FROM `second_table` GROUP BY score;
+FROM `second_table` GROUP BY score ORDER BY `number` DESC;
