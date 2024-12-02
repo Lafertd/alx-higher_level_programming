c6b4988e8b2b6878f050d07d2fc6b4d9dcc23582
lafertd <achrafprofessionalinfo@gmail.com>
2024-12-02 02:57:41 +0100

    updated

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 --- List all records NOT NULL DESC
-SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT NULL ORDER BY `score` DESC;
+SELECT `score`, `name` FROM `second_table` 
+WHERE `name` IS NOT NULL ORDER BY `score` DESC;
